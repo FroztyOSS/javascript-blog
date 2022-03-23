cb088677f4f021c639d3bd7013893058e46b269e
Łukasz Mróz <lmroz95@gmail.com>
2022-03-23 22:27:20 +0100

    Add next code to script.js and changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
 function titleClickHandler(event){
+     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log (event);
 
-  /* [DONE]remove class 'active' from all article links  */
+
+  /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -13,8 +15,8 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS]add class 'active' to the clicked link */
-  
+  /* [DONE] add class 'active' to the clicked link */
+   
   clickedElement.classList.add('active')
   console.log('clickedElement:', clickedElement);
   
@@ -28,11 +30,21 @@ function titleClickHandler(event){
   }
 
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector', articleSelector);
+
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector)
+  console.log('targetArticle', targetArticle);
 
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active')
+  console.log('targetArticle:', targetArticle);
+
 }
 
 const links = document.querySelectorAll('.titles a');
