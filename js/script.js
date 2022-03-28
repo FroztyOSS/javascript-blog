c3aba59ba3633e53d6d7275273882441eeba2f68
Łukasz Mróz <lmroz95@gmail.com>
2022-03-28 20:51:16 +0200

    create the HTML code for the link in script.js

--- js/script.js
+++ js/script.js
@@ -52,6 +52,7 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
@@ -60,21 +61,38 @@ function generateTitleLinks(){
 
   /* remove contents of titleList */
 
-  const titleList = document.querySelector(optTitleListSelector)
+  const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
+  console.log('titleList', titleList);
 
   /* for each article */
 
+const articles = document.querySelectorAll(optArticleSelector);
+  console.log('articles', articles);
+  console.log('optArticleSelector', optArticleSelector);
+
+for(let article of articles){
+
     /* get the article id */
+
+  const articleId = article.getAttribute('id');
+    console.log('articleId', articleId);
 
     /* find the title element */
 
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
     /* get the title from the title element */
 
+
+
     /* create HTML of the link */
+    
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML', linkHTML);
 
     /* insert link into titleList */
 
 }
-
+}
 generateTitleLinks();
